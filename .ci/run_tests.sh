a05e90469f784784ac77f7d5cec0d634d5a8a289
Ali Ghanavatian <ghanavatian.ali@gmail.com>
2019-11-11 14:03:36 +0330

    Update run_tests.sh

--- .ci/run_tests.sh
+++ .ci/run_tests.sh
@@ -8,9 +8,24 @@ if [ "$TEST_SUITE" == "lint" ]; then
 elif [ "$TEST_SUITE" == "unit" ]; then
   make test
 else
-#  createuser --createdb kong
-  sudo -u postgres psql -c "create role kong with createdb login password '123';"
-  createdb -U kong kong_tests
+  #  createuser --createdb kong
+#  psql -U postgres -c "create role kong with createdb login password '123';"
+#  createdb -U kong kong_tests
+
+  # POSTGRES
+  psql <<EOQ
+  DROP DATABASE IF EXISTS kong_tests;
+  DROP ROLE IF EXISTS kong;
+  CREATE USER kong WITH createdb password '123';
+  CREATE DATABASE kong_tests OWNER kong;
+EOQ
+
+  # CASSANDRA
+  cqlsh -u kong -p 123 --execute "DROP KEYSPACE IF EXISTS kong_tests;"
+  cqlsh -u kong -p 123 --execute "CREATE KEYSPACE IF NOT EXISTS kong_tests  WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };"
+
+  pwd
+  ../bin/kong migrations bootstrap -c ../spec/kong_tests.conf
 
   if [ "$TEST_SUITE" == "integration" ]; then
     make test-integration
