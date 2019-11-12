e21b2857169615da9c09bf92cea6356fe6495147
Ali Ghanavatian <ghanavatian.ali@gmail.com>
2019-11-12 19:38:01 +0330

    fix luacheck

--- .ci/setup_env.sh
+++ .ci/setup_env.sh
@@ -75,7 +75,7 @@ fi
 export PATH=$PATH:$OPENRESTY_INSTALL/nginx/sbin:$OPENRESTY_INSTALL/bin:$LUAROCKS_INSTALL/bin
 
 # luarocks install kong "$KONG_VERSION"-0
-luarocks install luacheck 0.23.0-1
+luarocks install luacheck 0.23.0-1 --local
 
 # # -------------------------------------
 # # Install ccm & setup Cassandra cluster
