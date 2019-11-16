ce0ebecfded933dbac2c5e6387aed0de9b39fd7e
Ali Ghanavatian <ghanavatian.ali@gmail.com>
2019-11-16 18:08:49 +0330

    Update run_tests.sh

--- .ci/run_tests.sh
+++ .ci/run_tests.sh
@@ -27,7 +27,7 @@ EOQ
   cqlsh -u kong -p 123 --execute "CREATE KEYSPACE IF NOT EXISTS kong_tests  WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };"
 
   echo "kong is bootstraping..."
-  kong migrations bootstrap -c ./spec/kong_tests.conf
+  /bin/kong migrations bootstrap -c ./spec/kong_tests.conf
   echo "kong bootstrapped."
   
   if [ "$TEST_SUITE" == "integration" ]; then
