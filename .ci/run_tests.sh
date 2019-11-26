0ec3a57068557fed6694fb39f9a941758b127c6c
admin <55698241+chamedoon-admin@users.noreply.github.com>
2019-11-26 13:34:35 +0330

    Update run_tests.sh

--- .ci/run_tests.sh
+++ .ci/run_tests.sh
@@ -34,7 +34,9 @@ EOQ
   which kong
   cat ./spec/kong_tests.conf
   echo "end of cat!!!!"
-  kong
+  kong roar
+  kong check
+  kong health
   echo "now bootstraping..."
   kong migrations bootstrap -c ./spec/kong_tests.conf
   echo "kong bootstrapped."
