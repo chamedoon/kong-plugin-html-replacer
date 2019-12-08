b0397dd8e71b3799b10d05c774bfc7f8c0607819
Ali Ghanavatian <ghanavatian.ali@gmail.com>
2019-12-08 21:08:26 +0330

    Update setup_env.sh

--- .ci/setup_env.sh
+++ .ci/setup_env.sh
@@ -2,7 +2,28 @@ set -e
 
 export LUAROCKS_INSTALL=$CACHE_DIR/luarocks
 
+#  LUAROCKS_INSTALL                 Overrides the `./config --prefix` value (default is `<prefix>/luarocks`)
+
+#  LUAROCKS_DESTDIR                 Overrides the `make install DESTDIR` (default is `/`)
+
+#  OPENRESTY_INSTALL                Overrides the `./config --prefix` value (default is `--prefix/openresty)
+
+#  OPENRESTY_DESTDIR                Overrides the `make install DESTDIR` (default is `/`)
+
+#  OPENSSL_INSTALL                  Overrides the `./config --prefix` value (default is `--prefix/openssl)
+
+#  OPENRESTY_RPATH
+
 mkdir -p $CACHE_DIR
+
+
+cd $CACHE_DIR
+git clone https://github.com/Kong/openresty-build-tools.git
+cd openresty-build-tools
+./kong-ngx-build -p buildroot --openresty 1.13.6.2 --openssl 1.1.1b --luarocks 3.0.4 
+
+
+
 
 echo "deb https://kong.bintray.com/kong-deb `lsb_release -sc` main" | sudo tee -a /etc/apt/sources.list
 curl -o bintray.key https://bintray.com/user/downloadSubjectPublicKey?username=bintray
