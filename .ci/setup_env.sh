58e3b9d3589e21d504548a3e517d2c9f6fbb1645
Ali Ghanavatian <ghanavatian.ali@gmail.com>
2019-12-12 22:31:45 +0330

    Update setup_env.sh

--- .ci/setup_env.sh
+++ .ci/setup_env.sh
@@ -1,5 +1,21 @@
 set -e
 
+sudo apt-get -y install --no-install-recommends wget gnupg ca-certificates
+
+# import our GPG key:
+wget -O - https://openresty.org/package/pubkey.gpg | sudo apt-key add -
+
+# for installing the add-apt-repository command
+# (you can remove this package and its dependencies later):
+sudo apt-get -y install --no-install-recommends software-properties-common
+
+# add the our official APT repository:
+sudo add-apt-repository -y "deb http://openresty.org/package/ubuntu $(lsb_release -sc) main"
+
+# to update the APT index:
+sudo apt-get update
+sudo apt install -y openresty
+
 export LUAROCKS_INSTALL=$CACHE_DIR/setup-luarocks
 export LUAROCKS_DESTDIR=luarocks
 
