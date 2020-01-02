757e94aae71328e569a18224f17c2857c11f0c64
Ali Ghanavatian <ghanavatian.ali@gmail.com>
2020-01-02 20:33:45 +0330

    Update and rename _setup_env.sh to setup_env.sh

--- .ci/setup_env.sh
+++ .ci/setup_env.sh
@@ -1,8 +1,9 @@
 set -e
 
-export OPENRESTY_INSTALL=$CACHE_DIR/openresty
-export LUAROCKS_INSTALL=$CACHE_DIR/luarocks
+export KONG_BUILD_DIR=$CACHE_DIR/kong-ngx-build
+export BUILD_ROOT=$KONG_BUILD_DIR/buildroot
 export KONG_INSTALL=$CACHE_DIR/kong
+
 export KONG_BASE="kong-${KONG_VERSION}"
 
 mkdir -p $CACHE_DIR
@@ -10,61 +11,17 @@ mkdir -p $CACHE_DIR
 if [ ! "$(ls -A $CACHE_DIR)" ]; then
   # Not in cache
 
-  # ---------------
-  # Install OpenSSL
-  # ---------------
-  OPENSSL_BASE=openssl-$OPENSSL
-  curl -L http://www.openssl.org/source/$OPENSSL_BASE.tar.gz | tar xz
-  # curl -L https://www.openssl.org/source/old/1.0.2/openssl-1.0.2h.tar.gz | tar xz
-  
+
   # -----------------
-  # Install OpenResty
-  # -----------------
-  OPENRESTY_BASE=openresty-$OPENRESTY
-  mkdir -p $OPENRESTY_INSTALL
-  curl -L https://openresty.org/download/$OPENRESTY_BASE.tar.gz | tar xz
-
-  pushd $OPENRESTY_BASE
-    ./configure \
-      --prefix=$OPENRESTY_INSTALL \
-      --with-openssl=../$OPENSSL_BASE \
-      --with-ipv6 \
-      --with-pcre-jit \
-      --with-http_ssl_module \
-      --with-http_realip_module \
-      --with-http_stub_status_module \
-      --with-http_v2_module
-    make
-    make install
+  git clone https://github.com/Kong/openresty-build-tools.git
+  pushd openresty-build-tools
+  ./kong-ngx-build -p buildroot --openresty $OPENRESTY --openssl $OPENSSL --luarocks $LUAROCKS --pcre $PCRE
   popd
-
-  rm -rf $OPENRESTY_BASE
-
-
-  # ----------------
-  # Install Luarocks
-  # ----------------
-  
-  LUAROCKS_BASE=luarocks-$LUAROCKS
-  mkdir -p $LUAROCKS_INSTALL
-  git clone https://github.com/keplerproject/luarocks.git $LUAROCKS_BASE
-
-  pushd $LUAROCKS_BASE
-    git checkout v$LUAROCKS
-    ./configure \
-      --prefix=$LUAROCKS_INSTALL \
-      --lua-suffix=jit \
-      --with-lua=$OPENRESTY_INSTALL/luajit \
-      --with-lua-include=$OPENRESTY_INSTALL/luajit/include/luajit-2.1
-    make build
-    make install
-  popd
-
-  rm -rf $LUAROCKS_BASE
-
 fi
 
-export PATH=$PATH:$OPENRESTY_INSTALL/nginx/sbin:$OPENRESTY_INSTALL/bin:$LUAROCKS_INSTALL/bin
+# export PATH=$PATH:$OPENRESTY_INSTALL/nginx/sbin:$OPENRESTY_INSTALL/bin:$LUAROCKS_INSTALL/bin
+export PATH="$BUILD_ROOT/luarocks/bin:$BUILD_ROOT/openssl/bin:$BUILD_ROOT/openresty/bin:$PATH"
+export OPENSSL_DIR="$BUILD_ROOT/openssl"
 
 eval `luarocks path`
 
@@ -83,7 +40,7 @@ if [ ! "$(ls -A $KONG_INSTALL)" ]; then
   echo $KONG_BASE
   git clone https://github.com/Kong/kong.git $KONG_BASE
   pushd $KONG_BASE
-  git checkout 1.4.0
+  git checkout $KONG_VERSION
   make
   luarocks make
   make install
@@ -116,10 +73,6 @@ luarocks --version
 luacheck -v
 kong version
 echo "^^^ kong version ^^^"
-kong check
-kong roar
-kong health
-echo "................................................................................"
 
 # serf version
 pip install cassandra-driver --user
@@ -129,5 +82,3 @@ cd apache-cassandra-2.2.7 && sh ./bin/cassandra && cd ..
 export PATH="${PATH}:${PWD}/apache-cassandra-2.2.7/bin"
 sudo service postgresql start
 while ! cqlsh -u cassandra -p cassandra -e 'describe cluster' ; do sleep 1; done
-echo "lua version:"
-lua -v
